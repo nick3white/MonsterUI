b2d149a0559b26754a8c7d557c2dc9b5169d366b
Isaac Flath <Isaac.flath@gmail.com>
2025-02-09 21:37:50 -0500

    Update api_reference.py

--- docs/api_reference/api_reference.py
+++ docs/api_reference/api_reference.py
@@ -973,6 +973,19 @@ def ex_markdown():
 ### And a subheading
 
 > This is a blockquote
+
+This supports inline latex: $e^{\\pi i} + 1 = 0$ as well as block latex thanks to Katex.
+
+$$
+\\frac{1}{2\\pi i} \\oint_C \\frac{f(z)}{z-z_0} dz
+$$
+
+And even syntax highlighting thanks to Highlight.js!
+
+```python
+def add(a, b):
+    return a + b
+```
 '''
     return render_md(md)
 
